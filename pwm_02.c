afa89c2f520b0541ee01b2f90ef7567ca75a8a7f
LuizEduardo-cyber <luizeduardosoaresferreira942@gmail.com>
2025-02-08 16:41:16 -0300

    adicionando os comentarios explicando o código

--- pwm_02.c
+++ pwm_02.c
@@ -2,8 +2,7 @@
 #include "pico/stdlib.h" //subconjunto central de bibliotecas do SDK Pico
 #include "hardware/pwm.h" //biblioteca para controlar o hardware de PWM
 
-//agora vamos definir os angulos
-
+//definir os angulos
 #define ang_180 6000 //0,12 * 50000 --> wrap
 #define ang_90 3675  //0,0735 * 50000 --> wrap
 #define ang_0 1250 //0,025 * 50000 --> wrap
@@ -14,7 +13,6 @@
 const uint16_t WRAP_PERIOD = 50000; //valor máximo do contador - WRAP
 const float PWM_DIVISER = 50.0; //divisor do clock para o PWM
 const uint16_t LED_STEP = 12.5; // 5 us --> 20000 us/ 5us = 4000 --> WRAP / 4000 -> 50000/4000 -> 12.5
-uint16_t led_level = ang_0; //nível inicial do pwm (duty cycle)
 
 
 
@@ -43,36 +41,36 @@ int main()
 
     uint up_down = 1; //variável para controlar se o nível do LED aumenta ou diminui
 
-    pwm_set_gpio_level(PWM_motor, ang_180);
-    sleep_ms(5000);
+    pwm_set_gpio_level(PWM_motor, ang_180); // deixa o motor no angulo 180°
+    sleep_ms(5000); //pausa de 5 seg
 
-    pwm_set_gpio_level(PWM_motor, ang_90);
-    sleep_ms(5000);
+    pwm_set_gpio_level(PWM_motor, ang_90); // deixa o motor no angulo 90°
+    sleep_ms(5000); //pausa de 5 seg
 
 
-    pwm_set_gpio_level(PWM_motor, ang_0);
-    sleep_ms(5000);
+    pwm_set_gpio_level(PWM_motor, ang_0); // deixa o motor no angulo 0°
+    sleep_ms(5000); //pausa de 5 seg
 
-    uint nivel_ang0 = ang_0;
+    uint nivel_ang0 = ang_0; //foi definido para começar o loop principal no angulo 0°
 
 
     //loop principal
     while (true) {
-        pwm_set_gpio_level(PWM_motor, nivel_ang0);
+        pwm_set_gpio_level(PWM_motor, nivel_ang0); //para setar o motor no angulo nivel_ang0
         if (up_down) 
         {
-            nivel_ang0 += LED_STEP; // Incrementa o nível do LED
+            nivel_ang0 += LED_STEP; // Incrementa para que ocorra o movimento do motor
             if (nivel_ang0 >= ang_180)
-                up_down = 0; // Muda direção para diminuir quando atingir o período máximo
+                up_down = 0; // Muda direção para diminuir quando atingir o angulo máximo que é 180°
         }
         else
         {
-            nivel_ang0 -= LED_STEP; // Decrementa o nível do LED
+            nivel_ang0 -= LED_STEP; // Decrementa para que ocorra o movimento contrário do motor
             if (nivel_ang0 <= ang_0)
-                up_down = 1; // Muda direção para aumentar quando atingir o mínimo
+                up_down = 1; // Muda direção para aumentar quando atingir o mínimo qie é 0°
         }
         
-        sleep_ms(10);
+        sleep_ms(10); //uma pequena pausa de 10 milissegundos
 
     }
 }
